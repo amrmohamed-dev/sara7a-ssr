86af08bf02eaf4bc7a084990ca63fc31ca52ea37
Amr Mohammed <amrali13255@gmail.com>
2025-12-11 09:54:52 +0200

    fix(utils): correct timeSince calculation

--- utils/timeSince.js
+++ utils/timeSince.js
@@ -2,11 +2,11 @@ function timeSince(date) {
   const seconds = Math.floor((new Date() - date) / 1000);
 
   const intervals = [
-    { label: 'year', seconds: 31536000 },
-    { label: 'month', seconds: 2592000 },
-    { label: 'day', seconds: 86400 },
-    { label: 'hour', seconds: 3600 },
     { label: 'minute', seconds: 60 },
+    { label: 'hour', seconds: 3600 },
+    { label: 'day', seconds: 86400 },
+    { label: 'month', seconds: 2592000 },
+    { label: 'year', seconds: 31536000 },
   ];
 
   let result = 'just now';

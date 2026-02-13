19fe70a159a3492359d9f6d8bb7ac276a11ed944
Amr Mohammed <amrali13255@gmail.com>
2026-02-13 14:04:42 +0200

    refactor(auth): return 403 for unverified accounts

--- middlewares/checkVerified.js
+++ middlewares/checkVerified.js
@@ -2,7 +2,9 @@ import AppError from '../utils/error/appError.js';
 
 const checkVerified = (req, res, next) => {
   if (!req.user.isVerified) {
-    return next(new AppError('You need to verify your account first'));
+    return next(
+      new AppError('You need to verify your account first', 403),
+    );
   }
   next();
 };
